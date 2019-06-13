9f49b242e7c33d0513bf993edcde2005b7fa6f62
ttionya <git@ttionya.com>
2019-06-13 17:42:07 +0800

    style 格式化脚本

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-require('babel-polyfill');
+const Koa = require('koa')
+const ip2Region = require('./utils/ip2region')
 
-const Koa = require('koa');
-const ip2Region = require('./utils/ip2region');
+const app = new Koa()
+const query = ip2Region.create('./data/ip2region.db')
 
-const app = new Koa();
-const query = ip2Region.create('./data/ip2region.db');
-
-const ipv4Reg = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+const ipv4Reg = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
 
 
 app
-  .use(async(ctx) => {
-    let { ip = '' } = ctx.query;
+  .use(async (ctx) => {
+    let { ip = '' } = ctx.query
 
     /**
      * 返回数据类型
@@ -22,31 +20,28 @@ app
       code: 0,
       message: '',
       data: null
-    };
+    }
 
     if (ipv4Reg.test(ip)) {
-      let data = query.btreeSearchSync(ip);
+      let data = query.btreeSearchSync(ip)
 
-      // 有结果
       if (data) {
-        let regionArray = data.region.split('|');
-        let dataArray = regionArray.filter(v => v != 0);
+        // 有结果
+        let regionArray = data.region.split('|')
+        let dataArray = regionArray.filter(v => v != 0)
 
-        responseObj.data = dataArray.join(' - ');
+        responseObj.data = dataArray.join(' - ')
+      } else {
+        // 无结果
+        responseObj.code = 10002
+        responseObj.message = '未检索到对应 IP 归属'
       }
-
-      // 无结果
-      else {
-        responseObj.code = 10002;
-        responseObj.message = '未检索到对应 IP 归属';
-      }
-    }
-    else {
-      responseObj.code = 10001;
-      responseObj.message = '未检测到参数或参数不合法';
+    } else {
+      responseObj.code = 10001
+      responseObj.message = '未检测到参数或参数不合法'
     }
 
-    ctx.body = responseObj;
+    ctx.body = responseObj
   })
 
-  .listen(3000);
+  .listen(3000)
